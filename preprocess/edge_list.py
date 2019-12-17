dce81825c602ebc36d7c366547a754b08423f73d
Wade <johnny12150@gmail.com>
2019-12-17 14:52:37 +0800

    create edge list for deepwalk but it's too slow

---
+++ preprocess/edge_list.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import ast
+from tqdm import tqdm
+
+# dblp = pd.read_pickle('dblp_2011up_venue_renamed.pkl')
+# dblp.fillna(value={'references': ''}, inplace=True)
+
+# 先只用top 10 conference
+dblp = pd.read_csv('preprocess/edge/dblp_venue_top10.csv')
+dblp.fillna(value={'references': ''}, inplace=True)
+
+tqdm.pandas()
+
+resultlist = []
+for i, data in tqdm(dblp.iterrows(), total=dblp.shape[0], position=0, leave=True):
+    # link paper to reference
+    if data.references:  # avoid empty value
+        refs = ast.literal_eval(data.references)
+        for j, ref in enumerate(refs):
+            # create tuple for citation
+            paper_link = (data.id, ref)
+            rev_paper_link = (ref, data.id)
+            # avoid any duplicates
+            if paper_link not in resultlist and rev_paper_link not in resultlist:
+                resultlist.append(paper_link)
+    # link paper to venue
+    if data.venue_id:
+        # 根據年分來建立venue node
+        paper_link = (data.id, str(int(data.year))+str(int(data.venue_id)))
+        rev_paper_link = (str(int(data.year))+str(int(data.venue_id)), data.id)
+        # avoid any duplicates
+        if paper_link not in resultlist and rev_paper_link not in resultlist:
+            resultlist.append(paper_link)
+
+        # 主要的venue node (不考慮年分)
+        paper_link = (data.id, int(data.venue_id))
+        rev_paper_link = (int(data.venue_id), data.id)
+        # avoid any duplicates
+        if paper_link not in resultlist and rev_paper_link not in resultlist:
+            resultlist.append(paper_link)
+    # link paper to author
+    if data.authors:  # avoid empty value
+        aus = ast.literal_eval(data.authors)
+        for j, au in enumerate(aus):
+            paper_link = (data.id, au['id'])
+            rev_paper_link = (au['id'], data.id)
+            if paper_link not in resultlist and rev_paper_link not in resultlist:
+                resultlist.append(paper_link)
+    if i ==3:
+        break
+
+# the above section may take around 35 mins to run
+
+# write tuple to txt
+with open('preprocess/edge/papers_edge_list.txt', 'w') as f:
+    for t in resultlist:
+        line = ' '.join(str(x) for x in t)
+        f.write(line + '\n')
+
+
