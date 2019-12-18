f46b340cd0e6a67309f63eec15ca11faf116f82b
Wade <johnny12150@gmail.com>
2019-12-18 16:31:19 +0800

    find new author and venue id

--- model/baseline/NN.py
+++ model/baseline/NN.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import ast
 from keras.models import Model
 from keras.layers import Input, Dense, Embedding, concatenate
 
@@ -8,8 +9,28 @@ bert_abstract = 768
 emb_dim = 100
 
 dblp_top50_conf = pd.read_pickle('preprocess/edge/paper_2011up_venue50.pkl')
+pa = pd.read_pickle('preprocess/edge/paper_author.pkl')
+pv = pd.read_pickle('preprocess/edge/paper_venue.pkl')
+# print(dblp_top50_conf.year.value_counts())  # 各年度幾篇
+paperId_compare = dblp_top50_conf.loc[:, ['id', 'new_papr_id', 'venue_id']]  # id與new_id的對照表
+
+# print(dblp_top50_conf.new_papr_id.max())  # 找paper的max id
+
+# authors = []
+# for i, data in dblp_top50_conf.authors.iteritems():
+#     for j in ast.literal_eval(data):
+#         authors.append(j['id'])
+# print(len(authors))  # 透過算作者有幾個找max id, 395952個
+
+# paper_new_id + author_id 後即為venue_id
+vId_start = dblp_top50_conf.new_papr_id.max() + 395952
+# vId, pId 對照表 (從新paperId找其venueId與authorId)
+# dblp_top50_conf['new_aId'] = dblp_top50_conf['new_aId'] + 395952
+# dblp_top50_conf['new_vId'] = dblp_top50_conf['venue_id'] + vId_start
+
 # select 2018以前全部當train
-print(dblp_top50_conf.year.value_counts())
+train2017 = dblp_top50_conf.loc[dblp_top50_conf.year < 2018, ['new_papr_id', 'title', 'new_vId']]
+
 
 # https://stackoverflow.com/questions/41888085/how-to-implement-word2vec-cbow-in-keras-with-shared-embedding-layer-and-negative
 # 用一個network去逼近embedding
