9fba3399c49353efb18642d067bf314b43f904d2
Wade <johnny12150@gmail.com>
2019-12-18 17:29:05 +0800

    add data loader

--- model/baseline/NN.py
+++ model/baseline/NN.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import ast
+import numpy as np
 from keras.models import Model
 from keras.layers import Input, Dense, Embedding, concatenate
 
@@ -12,25 +13,19 @@ dblp_top50_conf = pd.read_pickle('preprocess/edge/paper_2011up_venue50.pkl')
 pa = pd.read_pickle('preprocess/edge/paper_author.pkl')
 pv = pd.read_pickle('preprocess/edge/paper_venue.pkl')
 # print(dblp_top50_conf.year.value_counts())  # 各年度幾篇
-paperId_compare = dblp_top50_conf.loc[:, ['id', 'new_papr_id', 'venue_id']]  # id與new_id的對照表
+dblp_top50_conf['new_vId'] = pv['new_venue_id']
+# FIXME train的時候只考慮第一作者 ?
+# https://stackoverflow.com/questions/20067636/pandas-dataframe-get-first-row-of-each-group
+dblp_top50_conf['new_first_aId'] = pa.groupby('new_papr_id').first()['new_author_id']  # 取每篇的第一作者
+paperId_compare = dblp_top50_conf.loc[:, ['id', 'new_papr_id']]  # id與new_id的對照表
 
-# print(dblp_top50_conf.new_papr_id.max())  # 找paper的max id
-
-# authors = []
-# for i, data in dblp_top50_conf.authors.iteritems():
-#     for j in ast.literal_eval(data):
-#         authors.append(j['id'])
-# print(len(authors))  # 透過算作者有幾個找max id, 395952個
-
-# paper_new_id + author_id 後即為venue_id
-vId_start = dblp_top50_conf.new_papr_id.max() + 395952
 # vId, pId 對照表 (從新paperId找其venueId與authorId)
 # dblp_top50_conf['new_aId'] = dblp_top50_conf['new_aId'] + 395952
 # dblp_top50_conf['new_vId'] = dblp_top50_conf['venue_id'] + vId_start
 
 # select 2018以前全部當train
-train2017 = dblp_top50_conf.loc[dblp_top50_conf.year < 2018, ['new_papr_id', 'title', 'new_vId']]
-
+train2017 = dblp_top50_conf.loc[dblp_top50_conf.year < 2018, ['new_papr_id', 'title', 'new_vId', 'new_first_aId']]
+test2018 = dblp_top50_conf.loc[dblp_top50_conf.year > 2018, ['new_papr_id', 'title', 'new_vId', 'new_first_aId']]
 
 # https://stackoverflow.com/questions/41888085/how-to-implement-word2vec-cbow-in-keras-with-shared-embedding-layer-and-negative
 # 用一個network去逼近embedding
@@ -39,16 +34,50 @@ authorId_emb = Input(shape=(emb_dim,))  # graph embedding的結果
 title_emb = Input(shape=(bert_title,))  # BERT的結果
 abstract_emb = Input(shape=(bert_abstract,))  # BERT的結果
 
-out_emb = Dense(emb_dim, activation='sigmoid')(concatenate([paperId_emb, authorId_emb]))
-model = Model(input=[paperId_emb, authorId_emb], output=out_emb)
+# all_in_one = Input(shape=(emb_dim*2+bert_title+bert_abstract,))
+all_in_one = Input(shape=(emb_dim*2,))
+
+# out_emb = Dense(emb_dim, activation='sigmoid')(concatenate([paperId_emb, authorId_emb, title_emb, abstract_emb]))
+d1 = Dense(512, activation='linear')(all_in_one)
+d2 = Dense(400, activation='linear')(d1)
+d3 = Dense(256, activation='linear')(d2)
+out_emb = Dense(emb_dim, activation='sigmoid')(d3)
+model = Model(input=all_in_one, output=out_emb)
 
 # 最後一層用sigmoid/ linear輸出100個units, loss用mae硬做
 model.compile(optimizer='rmsprop', loss='mae')
 
 # 找出跟NN predict出最相似的embedding當作要推薦cite的論文
 
+node_id = np.load('preprocess/edge/node_id.npy').astype(int)
+emb_2017 = np.load('preprocess/edge/deep_walk_vec.npy')
+# sort both np at the same time
+sorter = node_id.argsort()
+emb_2017_sorted = emb_2017[sorter]
+
+
+# todo data generator, 或是直接concat起來送進keras
+def embedding_loader(emb_data, file_len, batch_size=32):
+    while True:
+        batch_x = []
+        batch_y = []
+        batch_paths = np.random.choice(a=file_len, size=batch_size)
+        for batch_i in batch_paths:
+            # 找出該paper的所有資訊
+            emb_p = emb_data[batch_i]  # paper emb
+            # 根據新paper id 找出 aId, vId
+            vId, aId = dblp_top50_conf.loc[batch_i, ['new_vId', 'new_first_aId']]
+            emb = np.hstack(emb_data[[vId, aId], :])
+            batch_x += []
+            batch_y += [batch_y]
+        batch_x = np.array(batch_x)
+        batch_y = np.array(batch_y)
+        yield batch_x, batch_y
+
+
+batch = 32
 # 先用2018前全部 train推薦系統, 2019的test推薦效果
-train_history = model.fit()
+train_history = model.fit_generator(embedding_loader(emb_2017, dblp_top50_conf.shape[0], batch), epochs=1, steps_per_epoch=dblp_top50_conf.shape[0] / batch, verbose=1)
 
 test_history = model.predict()
 
