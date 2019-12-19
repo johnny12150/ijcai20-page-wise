903fa3a56bd916d0ceb330d3845b38c835c7eb85
Wade <johnny12150@gmail.com>
2019-12-19 12:54:42 +0800

    remove some one time paper author

--- model/baseline/NN.py
+++ model/baseline/NN.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import ast
 import numpy as np
+import pickle
 from keras.models import Model
-from keras.layers import Input, Dense, Embedding, concatenate
+from keras.layers import Input, Dense, concatenate
 
 bert_title = 768
 bert_abstract = 768
@@ -10,22 +10,25 @@ bert_abstract = 768
 emb_dim = 100
 
 dblp_top50_conf = pd.read_pickle('preprocess/edge/paper_2011up_venue50.pkl')
+remove_author = pd.read_pickle("preprocess/edge/p_a_delete_author.pkl")
 pa = pd.read_pickle('preprocess/edge/paper_author.pkl')
+# 刪掉篇數太少的作者
+keep_author = remove_author.new_author_id.value_counts().index.values
+pa = pa.loc[pa.new_author_id.isin(keep_author), :]
 pv = pd.read_pickle('preprocess/edge/paper_venue.pkl')
-# print(dblp_top50_conf.year.value_counts())  # 各年度幾篇
+# ab = pd.read_pickle('preprocess/edge/abstracts.pkl')  # FIXME 編碼不對, 且空值未做處理
 dblp_top50_conf['new_vId'] = pv['new_venue_id']
 # FIXME train的時候只考慮第一作者 ?
 # https://stackoverflow.com/questions/20067636/pandas-dataframe-get-first-row-of-each-group
 dblp_top50_conf['new_first_aId'] = pa.groupby('new_papr_id').first()['new_author_id']  # 取每篇的第一作者
-paperId_compare = dblp_top50_conf.loc[:, ['id', 'new_papr_id']]  # id與new_id的對照表
 
-# vId, pId 對照表 (從新paperId找其venueId與authorId)
-# dblp_top50_conf['new_aId'] = dblp_top50_conf['new_aId'] + 395952
-# dblp_top50_conf['new_vId'] = dblp_top50_conf['venue_id'] + vId_start
+# paperId_compare = dblp_top50_conf.loc[:, ['id', 'new_papr_id']]  # id與new_id的對照表
+
+# TODO 輸出成一個刪減後的paper csv
 
 # select 2018以前全部當train
 train2017 = dblp_top50_conf.loc[dblp_top50_conf.year < 2018, ['new_papr_id', 'title', 'new_vId', 'new_first_aId']]
-test2018 = dblp_top50_conf.loc[dblp_top50_conf.year > 2018, ['new_papr_id', 'title', 'new_vId', 'new_first_aId']]
+test2018 = dblp_top50_conf.loc[dblp_top50_conf.year >= 2018, ['new_papr_id', 'title', 'new_vId', 'new_first_aId']]
 
 # https://stackoverflow.com/questions/41888085/how-to-implement-word2vec-cbow-in-keras-with-shared-embedding-layer-and-negative
 # 用一個network去逼近embedding
@@ -52,8 +55,12 @@ model.compile(optimizer='rmsprop', loss='mae')
 node_id = np.load('preprocess/edge/node_id.npy').astype(int)
 emb_2017 = np.load('preprocess/edge/deep_walk_vec.npy')
 # sort both np at the same time
-sorter = node_id.argsort()
-emb_2017_sorted = emb_2017[sorter]
+# sorter = node_id.argsort()
+# emb_2017_sorted = emb_2017[sorter]
+
+# line embedding
+with open('preprocess/edge/line_1and2.pkl', 'rb') as file:
+    line_emb = pickle.load(file)
 
 
 # todo data generator, 或是直接concat起來送進keras
@@ -63,24 +70,32 @@ def embedding_loader(emb_data, file_len, batch_size=32):
         batch_y = []
         batch_paths = np.random.choice(a=file_len, size=batch_size)
         for batch_i in batch_paths:
+            # TODO find node id 在emb_data的index
+
             # 找出該paper的所有資訊
-            emb_p = emb_data[batch_i]  # paper emb
+            emb_p = emb_data[str(batch_i)]  # paper emb
+            # emb_p = emb_data[batch_i]
             # 根據新paper id 找出 aId, vId
             vId, aId = dblp_top50_conf.loc[batch_i, ['new_vId', 'new_first_aId']]
-            emb = np.hstack(emb_data[[vId, aId], :])
-            batch_x += []
-            batch_y += [batch_y]
+            # emb = np.hstack(emb_data[[vId, aId], :])  # np style
+            emb1 = emb_data[str(vId)]
+            emb2 = emb_data[str(int(aId))]
+            emb = np.concatenate((emb1, emb2), axis=None)
+            batch_x += [emb]
+            batch_y += [emb_p]
         batch_x = np.array(batch_x)
         batch_y = np.array(batch_y)
+        print(batch_x.shape)
         yield batch_x, batch_y
 
 
 batch = 32
 # 先用2018前全部 train推薦系統, 2019的test推薦效果
-train_history = model.fit_generator(embedding_loader(emb_2017, dblp_top50_conf.shape[0], batch), epochs=1, steps_per_epoch=dblp_top50_conf.shape[0] / batch, verbose=1)
+# train_history = model.fit_generator(embedding_loader(emb_2017, train2017.new_papr_id.values), epochs=1, steps_per_epoch=train2017.shape[0] / batch, verbose=1)
+train_history = model.fit_generator(embedding_loader(line_emb, train2017.new_papr_id.values), epochs=1, steps_per_epoch=train2017.shape[0] / batch, verbose=1)
 
-test_history = model.predict()
+# test_history = model.predict()
 
 # TODO rolling的方式讓NN去學習embedding, for loop分年fit
-for i in range(8):
-    model.fit()
+# for i in range(8):
+#     model.fit()
